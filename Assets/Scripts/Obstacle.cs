baf5f504d6406ebe116138e308be410a58a08af2
Rumnn <nguyentiendoanh2k5@gmail.com>
2026-03-23 20:47:02 +0700

    first commit

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,34 @@
+using System.Numerics;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    Rigidbody2D rb;
+    public float minSpeed = 50f;
+    public float maxSpeed = 150f;
+    public float maxSpinSpeed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new UnityEngine.Vector3(randomSize, randomSize, 1);
+        rb = GetComponent<Rigidbody2D>();
+        
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        randomSpeed = randomSpeed / randomSize;
+        UnityEngine.Vector2 randomDirection = Random.insideUnitCircle;
+        rb.AddForce(randomDirection * randomSpeed);
+
+        float randomTorque = Random.Range(-maxSpinSpeed, maxSpinSpeed);
+        rb.AddTorque(randomTorque);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    private float elapsedTime = 0f;
+    private float score = 0f;
+    public float scoreMultiplier = 1f;
+    
+    public float thrustForce = 1f;
+    public float maxSpeed = 5f;
+
+    public GameObject boosterFlame;
+    Rigidbody2D rb;
+
+    public UIDocument uiDocument;
+    private Label scoreText;
+    private Button restartButton;
+
+    public GameObject explosionEffect;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
+        restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
+        restartButton.style.display = DisplayStyle.None;
+        restartButton.clicked += ReloadScene;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
+        Debug.Log("Score: " + score);
+        scoreText.text = "Score: " + score;
+
+        if (Mouse.current.leftButton.isPressed)
+        {
+            // Calculate mouse direction
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value) ;
+            Vector2 direction = (mousePos - transform.position).normalized;
+            
+            //move player in direction of mouse
+            transform.up = direction;
+            rb.AddForce(direction * thrustForce);
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+        }
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            boosterFlame.SetActive(true);
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            boosterFlame.SetActive(false);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        restartButton.style.display = DisplayStyle.Flex;
+        
+    }
+    void ReloadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
